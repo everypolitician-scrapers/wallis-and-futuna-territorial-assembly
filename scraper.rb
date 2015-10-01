1e41566114df157cbc1d568634bd9108a53430e4
Tony Bowden <tony@tmtm.com>
2015-10-01 12:56:15 +0100

    initial scraper

---
+++ scraper.rb
@@ -0,0 +1,42 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'scraperwiki'
+require 'nokogiri'
+require 'colorize'
+require 'pry'
+require 'open-uri/cached'
+OpenURI::Cache.cache_path = '.cache'
+
+class String
+  def tidy
+    self.gsub(/[[:space:]]+/, ' ').strip
+  end
+end
+
+def noko_for(url)
+  Nokogiri::HTML(open(url).read)
+end
+
+def scrape_list(url)
+  noko = noko_for(url)
+  composition = noko.xpath('//h2[.//span[@id="Composition"]]')
+  composition.xpath('following-sibling::h2 | following-sibling::h3').slice_before { |e| e.name == 'h2' }.first.each do |maj_or_opp|
+    maj_or_opp.xpath('following-sibling::h2 | following-sibling::h3 | following-sibling::h4').slice_before { |e| e.name != 'h4' }.first.each do |grp|
+      group = grp.css('.mw-headline').text.split(/\(/).first.tidy
+      grp.xpath('following-sibling::ul[1]/li').each do |li|
+        person = li.css('a').first
+        data = { 
+          name: person.text.tidy,
+          wikiname: person.attr('class') == 'new' ? '' : person.attr('title'),
+          area: li.text.split(',').last(2).join(', ').sub(')','').tidy,
+          party: group,
+          term: 2012,
+        }
+        ScraperWiki.save_sqlite([:name, :area, :party], data)
+      end
+    end
+  end
+end
+
+scrape_list('https://fr.wikipedia.org/wiki/Assembl%C3%A9e_territoriale_des_%C3%AEles_Wallis_et_Futuna')
